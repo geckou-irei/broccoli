7cc07318a9f49d7fc2f0686ef30d7ec747ee0bf8
geckou-irei <taishi.irei@geckou.net>
2022-08-18 23:48:39 +0900

    package.json 修正

--- nuxt.config.js
+++ nuxt.config.js
@@ -45,10 +45,5 @@ export default {
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
     transpile: ["gsap"],
-    extend(config, { isDev }) {
-      if (!isDev) {
-        config.output.publicPath = "./static/";
-      }
-    },
   },
 };
